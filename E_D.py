ccf0c3561caf5c66d10b13ab1d7105f634b61e34
AHazimy <99876551+AHazimy@users.noreply.github.com>
2022-06-01 23:36:49 +0300

    Type the general code

--- E_D.py
+++ E_D.py
@@ -1,50 +1,65 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 from hashlib import md5
 from Cryptodome.Cipher import AES
 from os import urandom
-
-def derive_key_and_iv(password, salt, key_length, iv_length): #derive key and IV from password and salt.
-    d = d_i = b''
-    while len(d) < key_length + iv_length:
-        d_i = md5(d_i + str.encode(password) + salt).digest() #obtain the md5 hash value
-        d += d_i
-    return d[:key_length], d[key_length:key_length+iv_length]
-
-def encrypt(in_file, out_file, password, key_length=32):
-    bs = AES.block_size #16 bytes
-    salt = urandom(bs) #return a string of random bytes
-    key, iv = derive_key_and_iv(password, salt, key_length, bs)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    out_file.write(salt)
-    finished = False
-
-    while not finished:
-        chunk = in_file.read(1024 * bs) 
-        if len(chunk) == 0 or len(chunk) % bs != 0:#final block/chunk is padded before encryption
-            padding_length = (bs - len(chunk) % bs) or bs
-            chunk += str.encode(padding_length * chr(padding_length))
-            finished = True
-        out_file.write(cipher.encrypt(chunk))
-
-def decrypt(in_file, out_file, password, key_length=32):
-    bs = AES.block_size
-    salt = in_file.read(bs)
-    key, iv = derive_key_and_iv(password, salt, key_length, bs)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    next_chunk = ''
-    finished = False
-    while not finished:
-        chunk, next_chunk = next_chunk, cipher.decrypt(in_file.read(1024 * bs))
-        if len(next_chunk) == 0:
-            padding_length = chunk[-1]
-            chunk = chunk[:-padding_length]
-            finished = True 
-        out_file.write(bytes(x for x in chunk)) 
+from MainWindow import Ui_MainWindow
 
 
-password = '12345' #shouldn't be something this simple
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        
+    password = '12345'    
+        
+    def derive_key_and_iv(password, salt, key_length, iv_length): #derive key and IV from password and salt.
+        d = d_i = b''
+        while len(d) < key_length + iv_length:
+            d_i = md5(d_i + str.encode(password) + salt).digest() #obtain the md5 hash value
+            d += d_i
+        return d[:key_length], d[key_length:key_length+iv_length]
 
-# with open('Qtree.xlsx', 'rb') as in_file, open('outfile.xlsx', 'wb') as out_file:
-#     encrypt(in_file, out_file, password)
+    def encrypt(in_file, out_file, password, key_length=32):
+        global bs
+        bs = AES.block_size #16 bytes
+        global salt
+        salt = urandom(bs) #return a string of random bytes
+        key, iv = self.derive_key_and_iv(password, salt, key_length, bs)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        out_file.write(salt)
+        finished = False
 
-with open('outfile.xlsx', 'rb') as in_file, open('outfile_decrypted.xlsx', 'wb') as out_file:
-    decrypt(in_file, out_file, password)
+        while not finished:
+            chunk = in_file.read(1024 * bs) 
+            if len(chunk) == 0 or len(chunk) % bs != 0:#final block/chunk is padded before encryption
+                padding_length = (bs - len(chunk) % bs) or bs
+                chunk += str.encode(padding_length * chr(padding_length))
+                finished = True
+            out_file.write(cipher.encrypt(chunk))
+
+    def decrypt(in_file, out_file, password, key_length=32):
+        bs = AES.block_size
+        salt = in_file.read(bs)
+        key, iv = self.derive_key_and_iv(password, salt, key_length, bs)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        next_chunk = ''
+        finished = False
+        while not finished:
+            chunk, next_chunk = next_chunk, cipher.decrypt(in_file.read(1024 * bs))
+            if len(next_chunk) == 0:
+                padding_length = chunk[-1]
+                chunk = chunk[:-padding_length]
+                finished = True 
+            out_file.write(bytes(x for x in chunk)) 
+
+
+     #shouldn't be something this simple
+    def run_enc(self, password, src_path, dest_path):
+        with open(src_path, 'rb') as in_file, open(dest_path, 'wb') as out_file:
+            self.encrypt(in_file, out_file, password)
+
+    def run_dec(self, password, src_path,dest_path):
+        with open(src_path, 'rb') as in_file, open(dest_path, 'wb') as out_file:
+            self.decrypt(in_file, out_file, password)
---
+++ MainWindow.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'MainWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(294, 279)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.lineEdit_source = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_source.setGeometry(QtCore.QRect(22, 30, 151, 20))
+        self.lineEdit_source.setObjectName("lineEdit_source")
+        self.lineEdit_destination = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_destination.setGeometry(QtCore.QRect(22, 100, 151, 20))
+        self.lineEdit_destination.setObjectName("lineEdit_destination")
+        self.btn_browse_source = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_browse_source.setGeometry(QtCore.QRect(190, 30, 75, 23))
+        self.btn_browse_source.setObjectName("btn_browse_source")
+        self.btn_browse_destination = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_browse_destination.setGeometry(QtCore.QRect(190, 100, 75, 23))
+        self.btn_browse_destination.setObjectName("btn_browse_destination")
+        self.btn_run = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_run.setGeometry(QtCore.QRect(20, 200, 241, 23))
+        self.btn_run.setObjectName("btn_run")
+        self.rb_encrypt = QtWidgets.QRadioButton(self.centralwidget)
+        self.rb_encrypt.setGeometry(QtCore.QRect(30, 160, 82, 17))
+        self.rb_encrypt.setObjectName("rb_encrypt")
+        self.rb_decrypt = QtWidgets.QRadioButton(self.centralwidget)
+        self.rb_decrypt.setGeometry(QtCore.QRect(190, 160, 82, 17))
+        self.rb_decrypt.setObjectName("rb_decrypt")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 294, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "AES_files"))
+        self.btn_browse_source.setText(_translate("MainWindow", "Browse"))
+        self.btn_browse_destination.setText(_translate("MainWindow", "Browse"))
+        self.btn_run.setText(_translate("MainWindow", "Run"))
+        self.rb_encrypt.setText(_translate("MainWindow", "Encrypt"))
+        self.rb_decrypt.setText(_translate("MainWindow", "Decrypt"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
